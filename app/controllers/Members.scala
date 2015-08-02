f68b4ae6ecba225ad7a11c32d76ca8fb50192301
Sevdiyor <diyor_1994_3838@mail.ru>
2015-08-02 15:13:38 +0500

    work with members

---
+++ app/controllers/Members.scala
@@ -0,0 +1,65 @@
+package controllers
+
+import models.{MembersDBTables, MembersDB}
+import play.api.Logger
+import play.api.db.slick.Config.driver.simple._
+import play.api.db.slick._
+import play.api.mvc._
+
+import scala.slick.lifted.TableQuery
+
+class Members extends Controller{
+  val membersDB = TableQuery[MembersDBTables]
+
+  def membersList = DBAction { implicit rs =>
+    Logger.info(s"SHOW_ALL = ${membersDB.list}")
+    Ok(views.html.membersList(membersDB.list))
+  }
+
+  def showAddForm = DBAction { implicit rs =>
+    Ok(views.html.addMembers())
+  }
+
+  def addMembers = DBAction { implicit request =>
+    val formParams = request.body.asFormUrlEncoded
+    val name = formParams.get("name")(0)
+    val surname = formParams.get("surname")(0)
+    val secondname = formParams.get("secondname")(0)
+    val adress = formParams.get("adress")(0)
+    val login = formParams.get("login")(0)
+    val pass = formParams.get("pass")(0)
+    val confPass = formParams.get("confPass")(0)
+
+    val membersId = (membersDB returning membersDB.map(_.id)) += MembersDB(None, name, surname, secondname, adress, login, pass, confPass)
+    Redirect(routes.Members.membersList())
+  }
+
+
+  def remove(id: Int) = DBAction { implicit request =>
+    membersDB.filter(_.id === id).delete
+    Redirect(routes.Members.membersList())
+  }
+  def updateMembers(id: Int) = DBAction { implicit request =>
+    val formParams = request.body.asFormUrlEncoded
+    val name = formParams.get("name")(0)
+    val surname = formParams.get("surname")(0)
+    val secondname = formParams.get("secondname")(0)
+    val adress = formParams.get("adress")(0)
+    val login = formParams.get("login")(0)
+    val pass = formParams.get("pass")(0)
+    val confPass = formParams.get("confPass")(0)
+
+      val member = MembersDB(Some(id), name, surname, secondname, adress, login, pass, confPass)
+      membersDB.filter(_.id === id).update(member)
+
+      Redirect(routes.Members.membersList())
+    }
+
+    def editFormMembersList(memberId: Int) = DBAction { implicit request =>
+      val byId = membersDB.findBy(_.id)
+      val member = byId(memberId).list.head
+
+      Ok(views.html.editMember(member))
+    }
+
+}
---
+++ app/models/Members.scala
@@ -0,0 +1,34 @@
+package models
+
+import play.api.db.slick.Config.driver.simple._
+
+case class MembersDB(id: Option[Int],
+                     name: String,
+                     surname: String,
+                     secondname: String,
+                     adress: String,
+                     login: String,
+                     pass: String,
+                     confPass: String)
+class MembersDBTables(tag: Tag) extends Table[MembersDB](tag, "Members") {
+
+  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+
+  def name = column[String]("NAME", O.Default(""))
+
+  def surname = column[String]("SURNAME", O.Default(""))
+
+  def secondname = column[String]("SECONDNAME", O.Default(""))
+
+  def adress = column[String]("ADRESS", O.Default(""))
+
+  def login = column[String]("LOGIN", O.Default(""))
+
+  def pass = column[String]("PASS", O.Default(""))
+
+  def confPass = column[String]("CONFPASS", O.Default(""))
+
+  def * = (id.?, name, surname , secondname, adress, login, pass, confPass) <>(MembersDB.tupled,MembersDB.unapply _)
+
+}
+
