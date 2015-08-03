fdbeac2bbec4dabfcd605153fd9d73646042e284
Sevdiyor <diyor_1994_3838@mail.ru>
2015-08-03 15:58:27 +0500

    added 00.png

--- app/controllers/Members.scala
+++ app/controllers/Members.scala
@@ -16,6 +16,11 @@ class Members extends Controller{
     Ok(views.html.membersList(membersDB.list))
   }
 
+  def membersListAdmin = DBAction { implicit rs =>
+    Logger.info(s"SHOW_ALL = ${membersDB.list}")
+    Ok(views.html.membersListAdmin(membersDB.list))
+  }
+
   def showAddForm = DBAction { implicit rs =>
     Ok(views.html.addMembers())
   }
